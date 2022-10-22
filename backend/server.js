5f6b51f9f08cac8c4e9a579b94129fd1ec0863e5
Julio Soto <juliosotodev@gmail.com>
2022-10-21 23:51:40 -0600

    initial express server setup and env vars

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('helLlo fRiEnD')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = new express()
+
+app.listen(PORT,_= console.log(`APP Server Listening @PORT:${PORT}`))
+
