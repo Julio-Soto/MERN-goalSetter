235b8868c86a5f376108af1515e78e47e0f97b85
Julio Soto <juliosotodev@gmail.com>
2022-10-22 21:39:54 -0600

    json and error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const errorHandler = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = new express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended:false }))
+
 app.use('/api/goals',require('./routes/goalroutes'))
+app.use(errorHandler)
 
 app.listen(PORT,_= console.log(`APP Server Listening @PORT:${PORT}`))
 
