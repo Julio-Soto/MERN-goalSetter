27c2436836e35590a12fb55f1f696d674b9d5231
Julio Soto <juliosotodev@gmail.com>
2022-10-25 23:50:53 -0600

    JWT generation and transfer

--- backend/controlers/userControlers.js
+++ backend/controlers/userControlers.js
@@ -37,7 +37,8 @@ const registerUser = asyncHandler(async (req,res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user.id)
         })
     }else{
         res.status(400)
@@ -56,7 +57,8 @@ const loginUser = asyncHandler(async (req,res) => {
         res.status(200).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user.id)
         })        
     } else{
         res.status(400)
@@ -70,6 +72,12 @@ const loginUser = asyncHandler(async (req,res) => {
 const getMe = asyncHandler(async (req,res) => {
     res.json({message: 'User data display'})
 })
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET,{
+        expiresIn: '30d'
+    })
+}
 
 module.exports = {
     registerUser,
