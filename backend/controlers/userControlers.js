051fcba6b4d84dc962e1cc7743be762321346e83
Julio Soto <juliosotodev@gmail.com>
2022-10-25 22:44:00 -0600

    debugged register-route

--- backend/controlers/userControlers.js
+++ backend/controlers/userControlers.js
@@ -23,14 +23,14 @@ const registerUser = asyncHandler(async (req,res) => {
     }
 
     // Hash password
-    const salt = bcrypt.genSalt(10)
+    const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password,salt)
     
     //Create user
     const user = await User.create({
         name,
         email,
-        password: hashedPassword
+        password
     })
 
     if(user) {
