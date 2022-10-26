3cd1b7e49a0c43178870b0421386fa3602ef6256
Julio Soto <juliosotodev@gmail.com>
2022-10-25 23:33:27 -0600

    login endpoint functionality

--- backend/controlers/userControlers.js
+++ backend/controlers/userControlers.js
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async (req,res) => {
     const user = await User.create({
         name,
         email,
-        password
+        password : hashedPassword
     })
 
     if(user) {
@@ -43,15 +43,25 @@ const registerUser = asyncHandler(async (req,res) => {
         res.status(400)
         throw new Error('Invalid user data')
     }
-
-    res.json({message: 'Register User'})
 })
 
 // @desc Authenticate a user.
-//@route POST /api/users/login
-//@access Public 
+// @route POST /api/users/login
+// @access Public 
 const loginUser = asyncHandler(async (req,res) => {
-    res.json({message: 'login User'})
+    const {email,password} = req.body
+    const user = await User.findOne({email})
+
+    if(user && await (bcrypt.compare(password,user.password))){
+        res.status(200).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })        
+    } else{
+        res.status(400)
+        throw new Error('Invalid Credentials!')
+    }
 })
 
 // @desc Get user data.
