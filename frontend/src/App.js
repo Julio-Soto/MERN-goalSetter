db0393099e41a8359e47b424e0f090fea3a94a27
Julio Soto <juliosotodev@gmail.com>
2022-10-27 23:59:28 -0600

    react router boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/dashboard';
+import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
   return (
-    <div className="App">
-      <h2>HelLow FrIEnd!!</h2>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path="/" element={<Dashboard/>} />
+            <Route path="/" element={<Login/>} />
+            <Route path="/" element={<Register/>} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
