e441e1f2d8198e09768418a151c86381c577a596
Julio Soto <juliosotodev@gmail.com>
2022-10-28 19:19:56 -0600

    Finished Login page

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import { useState } from "react";
+import {FaUser} from 'react-icons/fa'
 
 function Login() {
+    const [formData, setFormData]  = useState({
+        email: '',
+        password: ''
+    })
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+        console.table(formData)
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+    }
+
+    const {email,password} = formData
+
     return (
-        <div>Login Page</div>
+        <>
+            <section className="heading">
+                <h1>
+                    <FaUser/> Login
+                </h1>
+                <p>Enter Your Email and Password</p>
+            </section>
+
+            <section className="form">
+                <form onSubmit={onSubmit}>
+                    <div className="form-group">
+                        <input type="email" className="form-control" id="email" name='email' value={email} placeholder='Enter Your Email' onChange={onChange} />
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control" id="password" name='password' value={password} placeholder='Enter Your password' onChange={onChange} />
+                    </div>
+
+                    <div className="form-group">
+                        <button type='submit' className="btn btn-block">Submit</button>
+                    </div>
+                </form>
+            </section>
+        </>
     )
 }
 
